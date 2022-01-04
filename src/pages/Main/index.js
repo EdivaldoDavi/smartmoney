23f56985edd91bf20b4ef0a9e8f7fa486f4579bd
Edivaldo Davi Severiano <severianoed@sap.sp.gov.br>
2022-01-04 17:42:24 -0300

    core

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,23 +1,17 @@
-import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
 
 import BalancePanel from '../../components/BalancePanel';
-import EntryList from '../../components/EntryList';
 import EntrySummary from '../../components/EntrySummary';
-import Colors from '../../styles/Colors';
-const Main = ({navigation}) => {
-  const entriesGrouped = [
-    {key: '1', description: 'Alimentação', amount: 201},
-    {key: '2', description: 'Combustível', amount: 12},
-    {key: '3', description: 'Aluguel', amount: 120},
-    {key: '4', description: 'Lazer', amount: 250},
-    {key: '5', description: 'Outros', amount: 1200},
-  ];
+import EntryList from '../../components/EntryList';
 
+import Colors from '../../styles/Colors';
+
+const Main = ({navigation}) => {
   return (
     <View style={styles.container}>
       <BalancePanel onNewEntryPress={() => navigation.navigate('NewEntry')} />
-      <EntrySummary entriesGrouped={entriesGrouped} />
+      <EntrySummary />
       <EntryList navigation={navigation} />
     </View>
   );
